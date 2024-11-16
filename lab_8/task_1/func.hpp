115d777b047bf0985b5a0342bf295659da228b12
VladMaksimum <114826721+VladMaksimum@users.noreply.github.com>
2024-11-16 12:37:32 +0200

    Update func.hpp

--- lab_8/task_1/func.hpp
+++ lab_8/task_1/func.hpp
@@ -9,6 +9,7 @@ bool read(int& n, int& cnt, std::string words[n_max]);
 void max_letters(int cnt, std::string words[n_max], int counter_letters[n_max]);
 void clean_words(int cnt, std::string words[n_max]);
 void sort(int cnt, std::string words[n_max], int counter_letters[n_max]);
+void delete_unsuitable_words(int& cnt, std::string words[n_max], int counter_letters[n_max]);
 
 
 void final_write(int n, int cnt, std::string words[n_max], int counter_letters[n_max]);
