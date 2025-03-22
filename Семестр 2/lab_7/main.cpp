d3e17a805acb636e4899ceabdd0a1bc369139ec6
VladMaksimum <belyaninvladislav@gmail.com>
2025-03-22 22:44:40 +0200

    start lab_7

--- Семестр 2/lab_7/main.cpp
+++ Семестр 2/lab_7/main.cpp
@@ -12,7 +12,7 @@ class vector<bool>
 {
     private:
         size_t len;
-        unsigned char* bv_values= new unsigned char[1];
+        unsigned char* bv_values;
     public:
         vector<bool>()
         {
@@ -26,48 +26,86 @@ class vector<bool>
                 unsigned char* tmp = new unsigned char[len/8+1];
                 std::copy(bv_values, bv_values + len/8, tmp);
 
-                tmp[len] = (unsigned char)value;
-                delete[] bv_values;
+                tmp[len/8] = (unsigned char)value;
+
+                if(len!=0)
+                    delete[] bv_values;
                 bv_values = tmp;
+            }
+            else if(value)
+            {
+                bv_values[len/8] |= 1 << (len%8);
+            }
+            len++;
+        }
+
+        bool getElement(int i)
+        {
+            if(len%8==0)
+            {
+                return bv_values[(len-i-1)/8] & (1 << (7-i%8));
+            }
+            else if(i<=len%8)
+            {
+                return bv_values[len/8] & (1 << (len%8 - i%8 -1));
             }
             else
             {
-                bv_values[len/8 -1] += value * 10 * (len%8);
+                return bv_values[(len-i-1)/8] & (1 << (8-(i%8)));
+            }
+            
+        }
+
+        void setElement(int i, bool value)
+        {
+            if(value)
+            {
+                if(len%8==0)
+                {
+                    bv_values[(len-i-1)/8] = bv_values[(len-i-1)/8] | (1 << (7-i%8));
+                }
+                else if(i<=len%8)
+                {
+                    bv_values[len/8] = bv_values[len/8] | (1 << (len%8 - i%8 -1));
+                }
+                else
+                {
+                    bv_values[(len-i-1)/8] = bv_values[(len-i-1)/8] | (1 << (8-(i%8)));
+                }
+            }
+            else
+            {
+                if(len%8==0)
+                {
+                    bv_values[(len-i-1)/8] = bv_values[(len-i-1)/8] ^ (1 << (7-i%8));
+                }
+                else if(i<=len%8)
+                {
+                    bv_values[len/8] = bv_values[len/8] ^ (1 << (len%8 - i%8 -1));
+                }
+                else
+                {
+                    bv_values[(len-i-1)/8] = bv_values[(len-i-1)/8] ^ (1 << (8-(i%8)));
+                }
             }
         }
 
-        friend std::ostream& operator<<(std::ostream& out, vector<bool>& obj);
-
 };
 
-std::ostream& operator<<(std::ostream& out, vector<bool>& obj)
-{
-    if(obj.len%8==0)
-    {
-        for(int i=obj.len/8; i>0; i--)
-        {
-            out << (int)obj.bv_values[i];
-        }
-    }
-    else
-        for(int i=0; i<obj.len/8+1; i++)
-        {
-            out << obj.bv_values[i];
-        }
-    return out;
-}
 
 int main()
 {
     vector<bool> test;
-    for(int i=0; i<8; i++)
+    for(int i=0; i<10; i++)
     {
         bool v;
         std::cin >> v;
         test.push_back(v);
     }
+    std::cout << test.getElement(2) << std::endl;
+    test.setElement(2,true);
 
-    std::cout << test;
+    std::cout << test.getElement(2);
 
 }
 
