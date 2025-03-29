2d3b504cd048797b13fb9e84261c851f7ac90133
VladMaksimum <114826721+VladMaksimum@users.noreply.github.com>
2025-03-29 12:38:37 +0200

    fix lab_7

--- Семестр 2/lab_7/main.cpp
+++ Семестр 2/lab_7/main.cpp
@@ -20,28 +20,6 @@ class vector<bool>
             bv_values[0] = 0;
         }
 
-        vector<bool>& operator=(vector<bool> other)
-        {
-            std::swap(len, other.len);
-            std::swap(bv_values, other.bv_values);
-            return *this;
-        }
-
-        vector<bool>(const vector<bool>& other)
-        {
-            len = other.len;
-            if(len%8==0)
-            {
-                bv_values = new unsigned char[len/8];
-                std::copy(other.bv_values, other.bv_values + other.len/8, bv_values);
-            }
-            else
-            {
-                bv_values = new unsigned char[len/8 +1];
-                std::copy(other.bv_values, other.bv_values + other.len/8 +1, bv_values);
-            }
-        }
-
         ~vector<bool>()
         {
             delete[] bv_values;
@@ -53,38 +31,16 @@ class vector<bool>
             {
                 unsigned char* tmp = new unsigned char[len/8+1];
 
-                bool carry = false;
-                //std::cerr << (int)tmp[0] << std::endl;
-                tmp[len/8] = bv_values[len/8] >> 7;
-                //std::cerr << (int)tmp[0] << " " << (int)bv_values[0] << std::endl;
-                for(int i=0; i<len/8; i++)
-                {
-                    //std::cerr << tmp[i] << std::endl;
-                    tmp[i] = (bv_values[i] << 1) | carry;
-                    carry = bv_values[i] >> 7;
-                    //std::cerr << tmp[i] << std::endl;
-                }
-
-                tmp[0] |= value;
+                std::copy(bv_values, bv_values + (len/8), tmp);
+                tmp[len/8] = value;
 
                 delete[] bv_values;
                 bv_values = tmp;
             }
-            else
+            else if(value)
             {
-                bool carry = false;
-                for(int i=0; i<len/8+1; i++)
-                {
-                    //std::cerr << carry << std::endl;
-                    bool tmp = bv_values[i] >> 7;
-                    bv_values[i] = (bv_values[i] << 1) | carry;
-                    carry = tmp;
-                    //std::cerr << (int)bv_values[i] << std::endl;
-                }
-
-                bv_values[0] |= value;
+                bv_values[len/8] |= (1 << len%8);
             }
-
             //std::cerr << (int)bv_values[0] << " " << (int)bv_values[1] << std::endl;
             len++;
         }
@@ -103,48 +59,94 @@ class vector<bool>
         {   
             if(value)
             {
-                bv_values[(i-1)/8] |= (1 << (i%8));
+                bv_values[(i)/8] |= (1 << (i%8));
             }
             else
             {
-                bv_values[(i-1)/8] &= ~(1 << (i%8));
+                bv_values[(i)/8] &= ~(1 << (i%8));
             }
         }
 
         void insert(int index, bool value)
         {
-            vector<bool> tmp;
-            for(int i=len-1; i>index; i--)
+            if(len%8==0)
             {
-                //std::cerr <<(int)this->getElement(i) << std::endl;
-                tmp.push_back(this->getElement(i));
+                unsigned char* tmp = new unsigned char[len/8 +1];
+
+                tmp[len/8] = 0;
+                for(int i=len; i>-1; i--)
+                {
+                    if(i!=index)
+                    {
+                        if((bv_values[(i-1)/8] >> ((i-1)%8)) & 1)
+                        {
+                            tmp[i/8] |= (1 << (i%8));
+                        }
+                        else
+                        {
+                            tmp[i/8] &= ~(1 << (i%8));
+                        }
+                    }
+                    else
+                    {
+                        if(value)
+                        {
+                            tmp[i/8] |= (1 << (i%8));
+                        }
+                        else
+                        {
+                            tmp[i/8] &= ~(1 << (i%8));
+                        }
+                    }
+                }
+
+                delete[] bv_values;
+                bv_values = tmp;
             }
-            //std::cerr << "here" << std::endl;
-            tmp.push_back(value);
-            for(int j=index-1; j>-1; j--)
+            else
             {
-                //std::cerr << (int)this->getElement(j) << std::endl;
-                tmp.push_back(this->getElement(j));
+                this->push_back(false);
+                for(int i=len-1; i>-1; i--)
+                {
+                    if(i!=index)
+                    {
+                        if((bv_values[(i-1)/8] >> ((i-1)%8)) & 1)
+                        {
+                            bv_values[i/8] |= (1 << (i%8));
+                        }
+                        else
+                        {
+                            bv_values[i/8] &= ~(1 << (i%8));
+                        }
+                    }
+                    else
+                    {
+                        if(value)
+                        {
+                            bv_values[i/8] |= (1 << (i%8));
+                        }
+                        else
+                        {
+                            bv_values[i/8] &= ~(1 << (i%8));
+                        }
+                    }
+                }
             }
-
-            *this = tmp;
-
+            len++;
         }
 
         void erase(int index)
         {
-            vector<bool> tmp;
-            for(int i=len-1; i>-1; i--)
+            int j=0;
+            for(int i=0; i<len; i++)
             {
                 if(i!=index)
                 {
-                    tmp.push_back(this->getElement(i));
-                    //std::cerr << this->getElement(i) << std::endl;
+                    this->setElement(j, this->getElement(i));
+                    j++;
                 }
-                
             }
-
-            *this = tmp;
+            len--;
         }
 
 };
